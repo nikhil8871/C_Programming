2b403819c3dc8a071b12ce433c0392bd073f1151
mhadev <nikhilsoni8815@gmail.com>
2024-07-13 18:04:35 +0530

    C Programming Sum Code

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+ int a,b;
+ printf("please enter first value\n");
+ scanf("%d",&a);
+ printf("enter second value\n");
+ scanf("%d",&b);
+ printf("The sum is %d\n",a+b);
+ return 0;
+}
